0b5f9634b876e1c4e960b756ce6ea74a38b7328f
jorystewart <37415682+jorystewart@users.noreply.github.com>
2018-05-23 13:56:01 -0600

    Add files via upload

--- appCompare.ps1
+++ appCompare.ps1
@@ -3,9 +3,9 @@ function Remove-Connection {
     Invoke-Command -ComputerName $target -Credential $cred -ScriptBlock { Unregister-PSSessionConfiguration -Name appcheck -Force}
 }
 Write-Host "`nThis script will get a list of installed software that is not included on the base image."
-$user = Read-Host -Prompt "`nPlease enter your username:"
+$user = Read-Host -Prompt "`nPlease enter your username"
 $cred = Get-Credential $user
-$target = Read-Host -Prompt "`nEnter name of computer to check:"
+$target = Read-Host -Prompt "`nEnter name of computer to check"
 #Tests the connection to the target
 Write-Host "`nVerifying connectivity..."
 if (!(Test-Connection -quiet $target)) {
@@ -34,19 +34,19 @@ catch {
     throw "`nThere was an error starting a PowerShell session on the remote host.`n"
 }
 #Gets list of installed software on the target
-Invoke-Command -Session $session -ScriptBlock {Get-ItemProperty HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\* | Select-Object DisplayName, DisplayVersion | Export-Csv -Path C:\Windows\Temp\apps.csv}
+Invoke-Command -Session $session -ScriptBlock {Get-ItemProperty HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*  | Where-Object {$_.DisplayName -notmatch ".*KB[0-9]{6,}"} | Select-Object DisplayName, DisplayVersion | Export-Csv -Path C:\Windows\Temp\apps.csv}
 Copy-Item -FromSession $session C:\Windows\Temp\apps.csv -Destination C:\Scripts\Temp\apps.csv
 #Compares installed software and reference list from base image
-Compare-Object -ReferenceObject $(Get-Content C:\Scripts\baseapps.csv) -DifferenceObject $(Get-Content C:\Scripts\Temp\apps.csv) | Export-Csv C:\Scripts\Temp\appdelta.csv
+Compare-Object -ReferenceObject $(Get-Content C:\Scripts\csv\baseappsver.csv) -DifferenceObject $(Get-Content C:\Scripts\Temp\apps.csv) | Export-Csv C:\Scripts\Temp\appdelta.csv
 (Get-Content C:\Scripts\Temp\appdelta.csv).replace('=>', 'Target') | Set-Content C:\Scripts\Temp\appdelta.csv
 (Get-Content C:\Scripts\Temp\appdelta.csv).replace('<=', "Base") | Set-Content C:\Scripts\Temp\appdelta.csv
 Write-Host "`n64-bit Software:`n"
 Import-Csv C:\Scripts\Temp\appdelta.csv | Format-Table -AutoSize
-Invoke-Command -Session $session -ScriptBlock {Get-ItemProperty HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | Select-Object DisplayName, DisplayVersion | Export-Csv -Path C:\Windows\Temp\apps32.csv}
+Invoke-Command -Session $session -ScriptBlock {Get-ItemProperty HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | Where-Object {$_.DisplayName -notmatch ".*KB[0-9]{6,}"} | Select-Object DisplayName, DisplayVersion | Export-Csv -Path C:\Windows\Temp\apps32.csv}
 Copy-Item -FromSession $session C:\Windows\Temp\apps32.csv -Destination C:\Scripts\Temp\apps32.csv
-Compare-Object -ReferenceObject $(Get-Content C:\Scripts\baseapps32.csv) -DifferenceObject $(Get-Content C:\Scripts\Temp\apps32.csv) | Export-Csv C:\Scripts\Temp\appdelta32.csv
+Compare-Object -ReferenceObject $(Get-Content C:\Scripts\csv\baseapps32ver.csv) -DifferenceObject $(Get-Content C:\Scripts\Temp\apps32.csv) | Export-Csv C:\Scripts\Temp\appdelta32.csv
 (Get-Content C:\Scripts\Temp\appdelta32.csv).replace('=>', 'Target') | Set-Content C:\Scripts\Temp\appdelta32.csv
 (Get-Content C:\Scripts\Temp\appdelta32.csv).replace('<=', "Base") | Set-Content C:\Scripts\Temp\appdelta32.csv
 Write-Host "`n32-bit Software`n"
-Import-Csv C:\Scripts\Temp\appdelta32.csv | Format-Table -AutoSize
+Import-Csv C:\Scripts\Temp\appdelta32.csv | where-object {$_.InputObject -notmatch "^.?$"} | Format-Table -AutoSize
 Remove-Connection
